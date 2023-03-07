4d916804e9fd2f5dc0e815963161d593c664739b
mdrahmatullahamn <rmd831219@gmail.com>
2023-03-07 16:44:35 +0530

    udate

--- pro.js
+++ pro.js
@@ -1,13 +1,13 @@
 window.onload = function () {
     // alert("warning")
     var vewier = JSON.parse(localStorage.getItem("product"));
-    var atdataLS = document.getElementById("some");
+    var atdataLS = document.getElementById("simple");
 
 
     
     var array = []
     for (var i = 0; i < vewier.length; i++) {
-        array += `<div><img src="${vewier[i].image}" alt=""><h1>${vewier[i].name}</h1> <p>${vewier[i].price}</p></div>`
+        array += `<div class="rockey"><img src="${vewier[i].image}" alt=""><h1>${vewier[i].name}</h1> <p>${vewier[i].price}</p></div>`
         // array += `<div class="m10" ><img src="${vewier[i].image}" alt=""><h1>${vewier[i].name}</h1> <p>${vewier[i].price}</p></div>`
         // array += `<div class="m10" ><img src="${vewier[i].image}" alt=""><h1>${vewier[i].name}</h1> <p>${vewier[i].price}</p></div>`
         // array += `<div class="m10" ><img src="${vewier[i].image}" alt=""><h1>${vewier[i].name}</h1> <p>${vewier[i].price}</p></div>`
